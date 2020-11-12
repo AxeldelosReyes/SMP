07a3a61a7726405ebc41764a7f1701b37a3984f4
AxeldelosReyes <shelonga@live.com>
2020-11-12 14:57:59 -0600

    first commit

---
+++ informe_po_spanish/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Xmarts Reporte orden de compra interm ',
+    'version': '12',
+    'category': "",
+    'description': """ Purchase Order in spanish Report 
+    """,
+    'author':'Saul',
+    'depends': ['base','purchase'],
+    'data': [
+        "report/po_spanish.xml",
+    ],
+    'qweb': [
+        ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ informe_quotation/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Xmarts Reporte cotizacion interm ',
+    'version': '12',
+    'category': "",
+    'description': """ Sales quotation Report 
+    """,
+    'author':'Saul',
+    'depends': ['base','sale'],
+    'data': [
+        "report/sale_quotation.xml",
+    ],
+    'qweb': [
+        ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
